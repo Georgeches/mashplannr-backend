aca39b6c0f84829f128596f964e9bf6e583ab5f0
Feisal Keli <105209149+feisalkeli@users.noreply.github.com>
2023-07-31 10:54:07 +0300

    auth done

--- db/migrate/20230728130238_add_password_digest_to_merchandesisers.rb
+++
@@ -1,6 +0,0 @@
-class AddPasswordDigestToMerchandesisers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :merchandisers, :password_digest, :string
-
-  end
-end
--- db/migrate/20230728130742_add_email_to_merchandisers.rb
+++
@@ -1,5 +0,0 @@
-class AddEmailToMerchandisers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :merchandisers, :email, :string
-  end
-end
